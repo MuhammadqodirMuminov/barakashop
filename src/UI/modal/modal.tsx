6a6ea22d722ea46ab39297234670d2ac5c3e4ccf
husanovjamshid <husanovjamshid11@gmail.com>
2023-12-04 00:31:11 +0500

    add modal

--- src/UI/modal/modal.tsx
+++ src/UI/modal/modal.tsx
@@ -1,3 +1,34 @@
-export const Modal = () => {
-	return <div>Modal</div>;
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  TagCloseButton,
+	Button
+} from "@chakra-ui/react";
+import { ModalProps } from "./modal.props";
+
+
+export const UiModal = ({ isOpen, onClose, title, children, closeTitle }:ModalProps) => {
+  return (
+    <>
+      {" "}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{title}</ModalHeader>
+          <TagCloseButton />
+          <ModalBody>{children}</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              {closeTitle}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
