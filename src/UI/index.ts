0c3c2fcf6093c20e880c30c3de96108fe27ca7a0
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-12-04 22:07:57 +0500

    swiper & slider ui add

--- src/UI/index.ts
+++ src/UI/index.ts
@@ -2,3 +2,4 @@ export { Loading } from './loading';
 export { PageTitle } from './page-title/page-title';
 export { CustomButton } from './button/button';
 export { ProductCard } from './card/card';
+export { Slider } from './slider/slider'
---
+++ src/UI/slider/slider.props.ts
@@ -0,0 +1,8 @@
+import { SwiperProps } from "swiper/react";
+
+
+
+export interface SliderProps extends SwiperProps {
+    items: JSX.Element[]
+    buttons?: boolean;
+}
