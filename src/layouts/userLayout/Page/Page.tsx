e3af59fd14f3756cf4665690ae74c0d2e4bf44d2
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2023-12-04 10:36:41 +0500

    config/fix

--- src/layouts/userLayout/Page/Page.tsx
+++ src/layouts/userLayout/Page/Page.tsx
@@ -1,12 +1,16 @@
-import { FC, PropsWithChildren } from 'react';
+import { Box } from '@chakra-ui/react';
+import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
 
-export const PageLayout: FC<PropsWithChildren> = ({ children }) => {
+export const PageLayout: FC = () => {
 	return (
 		<>
 			<Header />
-			<div>{children}</div>
+			<Box>
+				<Outlet />
+			</Box>
 			<Footer />
 		</>
 	);
