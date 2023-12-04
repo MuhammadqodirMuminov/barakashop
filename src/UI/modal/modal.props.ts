edded3f44eaacce902282f8b246e9a74617b579f
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2023-12-04 10:04:50 +0500

    stashed

--- src/UI/modal/modal.props.ts
+++ src/UI/modal/modal.props.ts
@@ -1,5 +1,5 @@
-export  interface ModalProps {
-	title: string,
-	children: string,
-	closeTitle: string
+export interface ModalProps {
+	title: string;
+	children: string;
+	closeTitle: string;
 }
--- src/UI/modal/modal.tsx
+++ src/UI/modal/modal.tsx
@@ -1,34 +1,24 @@
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  TagCloseButton,
-	Button
-} from "@chakra-ui/react";
-import { ModalProps } from "./modal.props";
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, TagCloseButton } from '@chakra-ui/react';
+import { FC, PropsWithChildren } from 'react';
+import { ModalProps } from './modal.props';
 
+export const UiModal: FC<PropsWithChildren<ModalProps>> = ({ isOpen, onClose, title, children, closeTitle }) => {
+	return (
+		<>
+			<Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>{title}</ModalHeader>
+					<TagCloseButton />
+					<ModalBody>{children}</ModalBody>
 
-export const UiModal = ({ isOpen, onClose, title, children, closeTitle }:ModalProps) => {
-  return (
-    <>
-      {" "}
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{title}</ModalHeader>
-          <TagCloseButton />
-          <ModalBody>{children}</ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              {closeTitle}
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  );
+					<ModalFooter>
+						<Button colorScheme='blue' mr={3} onClick={onClose}>
+							{closeTitle}
+						</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	);
 };
