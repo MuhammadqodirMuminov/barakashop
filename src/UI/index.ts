a0a529ea3c00a04cc3dceb8ccff6a93b22bcd660
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-12-05 14:33:38 +0500

    custom text import

--- src/UI/index.ts
+++ src/UI/index.ts
@@ -4,3 +4,4 @@ export { Loading } from './loading';
 export { PageTitle } from './page-title/page-title';
 export { ProductCard } from './card/card';
 export { Slider } from './slider/slider'
+export { CustomText } from './text/text'
