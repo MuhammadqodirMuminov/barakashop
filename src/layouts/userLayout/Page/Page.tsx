fda7ece4b74fc3b6f268930b9f37826468bb86f6
husanovjamshid <husanovjamshid11@gmail.com>
2023-12-05 17:30:29 +0500

    fix conflict main ant card tsx

--- src/layouts/userLayout/Page/Page.tsx
+++ src/layouts/userLayout/Page/Page.tsx
@@ -1,8 +1,6 @@
 import { FC, PropsWithChildren } from 'react';
-<<<<<<<<< Temporary merge branch 1
-=========
+
 import { Box } from '@chakra-ui/react';
->>>>>>>>> Temporary merge branch 2
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
