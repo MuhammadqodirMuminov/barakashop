fd9e3b8aaceca9de6b1123e834059b7ac8fc81b0
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2023-12-05 19:34:41 +0500

    pr: merge section-title

--- src/UI/section-title/section-title.tsx
+++ src/UI/section-title/section-title.tsx
@@ -1,56 +1,42 @@
-import { Box, Image } from '@chakra-ui/react';
-import { SectionTitleProps } from './section-title.props';
 import { CustomButton, CustomText } from '@/UI';
-import { Link } from 'react-router-dom';
+import { Box, Image } from '@chakra-ui/react';
 import rightArrowIcon from '@public/icons/arrows/right.svg';
+import { Link } from 'react-router-dom';
+import { SectionTitleProps } from './section-title.props';
 
-export const SectionTitle = ({
-    title,
-    link,
-    link_title,
-}: SectionTitleProps) => {
-    return (
-        <Box
-            display={'flex'}
-            alignItems={'center'}
-            justifyContent={'space-between'}
-            fontWeight={'bold'}
-        >
-            <CustomText
-                fontSize={{
-                    base: 12,
-                    sm: 15,
-                    md: 18,
-                    lg: 20,
-                    xl: 22,
-                }}
-            >
-                {title}
-            </CustomText>
-            <Link to={link}>
-                <CustomButton
-                    p={{
-                        base: 2,
-                        sm: 5,
-                        md: 6,
-                    }}
-                    color={'#7500d8'}
-                    text={link_title ? link_title : title}
-                    fontSize={{
-                        base: 12,
-                        sm: 14,
-                        md: 16,
-                        lg: 18,
-                        xl: 20,
-                    }}
-                    image={
-                        <Image
-                            ml={5}
-                            src={rightArrowIcon}
-                        />
-                    }
-                />
-            </Link>
-        </Box>
-    );
+export const SectionTitle = ({ title, link, link_title }: SectionTitleProps) => {
+	return (
+		<Box display={'flex'} alignItems={'center'} justifyContent={'space-between'} fontWeight={'bold'}>
+			<CustomText
+				fontSize={{
+					base: 12,
+					sm: 15,
+					md: 18,
+					lg: 20,
+					xl: 22,
+				}}
+			>
+				{title}
+			</CustomText>
+			<Link to={link}>
+				<CustomButton
+					p={{
+						base: 2,
+						sm: 5,
+						md: 6,
+					}}
+					color={'#1976d2'}
+					text={link_title ? link_title : title}
+					fontSize={{
+						base: 10,
+						sm: 12,
+						md: 14,
+						lg: 16,
+						xl: 18,
+					}}
+					image={<Image ml={5} src={rightArrowIcon} />}
+				/>
+			</Link>
+		</Box>
+	);
 };
