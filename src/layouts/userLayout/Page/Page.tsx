d191c3387075c4cc7ba57bdf5ededc2f837c6fd7
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-12-05 19:04:45 +0500

    section title undo

--- src/layouts/userLayout/Page/Page.tsx
+++ src/layouts/userLayout/Page/Page.tsx
@@ -2,14 +2,12 @@ import { FC, PropsWithChildren } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
-import { SectionTitle } from '@/UI/section-title/section-title';
 export const PageLayout: FC<PropsWithChildren> = () => {
     return (
         <>
             <Header />
             <Outlet />
             <Footer />
-            <SectionTitle title='Recomended' link='/product/rec' link_title='Recomended all'/>
         </>
     );
 };
