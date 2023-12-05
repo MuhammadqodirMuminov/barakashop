cafc322f7b6522b275333d6cc4d25f1c75340129
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-12-05 19:04:16 +0500

    section-title ui add

--- src/UI/section-title/section-title.tsx
+++ src/UI/section-title/section-title.tsx
@@ -1,3 +1,56 @@
-export const SectionTitle = () => {
-	return <div>SectionTitle</div>;
+import { Box, Image } from '@chakra-ui/react';
+import { SectionTitleProps } from './section-title.props';
+import { CustomButton, CustomText } from '@/UI';
+import { Link } from 'react-router-dom';
+import rightArrowIcon from '@public/icons/arrows/right.svg';
+
+export const SectionTitle = ({
+    title,
+    link,
+    link_title,
+}: SectionTitleProps) => {
+    return (
+        <Box
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'space-between'}
+            fontWeight={'bold'}
+        >
+            <CustomText
+                fontSize={{
+                    base: 12,
+                    sm: 15,
+                    md: 18,
+                    lg: 20,
+                    xl: 22,
+                }}
+            >
+                {title}
+            </CustomText>
+            <Link to={link}>
+                <CustomButton
+                    p={{
+                        base: 2,
+                        sm: 5,
+                        md: 6,
+                    }}
+                    color={'#7500d8'}
+                    text={link_title ? link_title : title}
+                    fontSize={{
+                        base: 12,
+                        sm: 14,
+                        md: 16,
+                        lg: 18,
+                        xl: 20,
+                    }}
+                    image={
+                        <Image
+                            ml={5}
+                            src={rightArrowIcon}
+                        />
+                    }
+                />
+            </Link>
+        </Box>
+    );
 };
--- src/layouts/userLayout/Page/Page.tsx
+++ src/layouts/userLayout/Page/Page.tsx
@@ -2,27 +2,14 @@ import { FC, PropsWithChildren } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
-import { Slider } from '@/UI';
+import { SectionTitle } from '@/UI/section-title/section-title';
 export const PageLayout: FC<PropsWithChildren> = () => {
     return (
         <>
             <Header />
             <Outlet />
             <Footer />
-            <Slider
-                items={[
-                    {
-                        title: 'Qaynoq yozda qaynoq aksiyalar',
-                        description: 'Olingan har bir mahsulot uchun chegirma',
-                        image: '',
-                    },
-                    {
-                        title: 'Qaynoq qishda qaynoq aksiyalar',
-                        description: 'Olingan har bir mahsulot uchun chegirma',
-                        image: '',
-                    },
-                ]}
-            />
+            <SectionTitle title='Recomended' link='/product/rec' link_title='Recomended all'/>
         </>
     );
 };
