e6d671a134cbd0f6eb4076b414e25c49178955dd
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2023-12-05 19:28:22 +0500

    updated ui

--- src/UI/rating/rating.tsx
+++ src/UI/rating/rating.tsx
@@ -1,31 +1,26 @@
-import filledStarIcon from '@public/icons/rating/filled.svg';
-import emptyStarIcon from '@public/icons/rating/empty.svg';
+import emptyStarIcon from '@/assets/icons/rating/empty.svg';
+import filledStarIcon from '@/assets/icons/rating/filled.svg';
 import { Image } from '@chakra-ui/react';
 
 export function ratingImageGenerate(rating: number): JSX.Element {
-    let totalRating: number = 0;
-    let ratingImages: string[] = [];
+	let totalRating: number = 0;
+	let ratingImages: string[] = [];
 
-    while (totalRating < 5) {
-        if (totalRating < Math.round(rating)) {
-            ratingImages.push(filledStarIcon);
-            totalRating += 1;
-        } else {
-            totalRating += 1;
-            ratingImages.push(emptyStarIcon);
-        }
-    }
+	while (totalRating < 5) {
+		if (totalRating < Math.round(rating)) {
+			ratingImages.push(filledStarIcon);
+			totalRating += 1;
+		} else {
+			totalRating += 1;
+			ratingImages.push(emptyStarIcon);
+		}
+	}
 
-    return (
-        <>
-            {ratingImages.map((item) => (
-                <Image
-                    mr={'2px'}
-                    key={item}
-                    src={item}
-                    alt='rating icon'
-                />
-            ))}
-        </>
-    );
+	return (
+		<>
+			{ratingImages.map(item => (
+				<Image mr={'2px'} key={item} src={item} alt='rating icon' />
+			))}
+		</>
+	);
 }
