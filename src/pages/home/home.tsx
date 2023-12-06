875d51f1940cc2bfd022a7745a5524ba5fbe2944
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-12-06 13:01:38 +0500

    category-card add

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,9 @@
-import { CategoryCard, SectionTitle } from '@/ui';
+import { SectionTitle } from '@/ui';
 
 export const Home = () => {
 	return (
 		<div>
 			<SectionTitle title='Recommend' link='/salom' link_title='hello' />
-			<CategoryCard category={{id: 1, categoty_image: '', title: 'Electronic', description: ''}}/>
 		</div>
 	);
 };
--- src/ui/card/category-card.tsx
+++ src/ui/card/category-card.tsx
@@ -2,26 +2,61 @@ import { Image } from '@chakra-ui/react';
 import { ICategoryCardProps } from './category-card-props';
 import { Box } from '@chakra-ui/layout';
 import { CustomButton, CustomText } from '@/ui';
-import CategoryBgi from '@/assets/img/placeholders/category-bg.png';
+import { CategoryCardBox, CategoryImage } from './category-card-styled';
+import { Link } from 'react-router-dom';
+import ArrowIcon from '@/assets/icons/arrows/white-arrow.svg';
+import CategoryThumb from '@/assets/img/placeholders/product.png';
 
 export const CategoryCard = ({ category }: ICategoryCardProps) => {
     const { id, title, categoty_image } = category;
 
     return (
-        <Box
-            display={'flex'}
-            bgImage={CategoryBgi}
-        >
+        <CategoryCardBox>
             <Box>
-                <CustomText>{title}</CustomText>
+                <CustomText
+                    fontSize={{
+                        base: 14,
+                        sm: 16,
+                        md: 20,
+                        lg: 22,
+                        xl: 24,
+                    }}
+                    fontWeight={'bold'}
+                    w={'100%'}
+                    maxW={{
+                        base: '150px',
+                    }}
+                >
+                    {title}
+                </CustomText>
                 <CustomText>
-                    <CustomButton
-                        text='Visit'
-                        image={<Image src='' />}
-                    />
+                    <Link to={`/product/${id}`}>
+                        <CustomButton
+                            mt={8}
+                            bgColor={'#023581'}
+                            color={'#ffffff'}
+                            fontSize={{
+                                base: 12,
+                                sm: 14,
+                                md: 16,
+                            }}
+                            text='Visit'
+                            _hover={{
+                                bgColor: '#023581d4',
+                            }}
+                            image={
+                                <Image
+                                    ml={4}
+                                    src={ArrowIcon}
+                                />
+                            }
+                        />
+                    </Link>
                 </CustomText>
             </Box>
-            <Image src='' />
-        </Box>
+            <CategoryImage
+                src={categoty_image ? categoty_image : CategoryThumb}
+            />
+        </CategoryCardBox>
     );
 };
