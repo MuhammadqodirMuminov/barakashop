cc6eebcae5e46b6c270bb5a0d26f507c785475ee
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-12-09 15:19:08 +0500

    category-page changed

--- src/layouts/user-layout/Page/Page.tsx
+++ src/layouts/user-layout/Page/Page.tsx
@@ -1,6 +1,6 @@
 import { FC, PropsWithChildren } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Footer } from '../footer/footer';
+import { Footer } from '../Footer/Footer';
 import { Header } from '../header';
 export const PageLayout: FC<PropsWithChildren> = () => {
 	return (
