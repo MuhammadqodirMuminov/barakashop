f99362ec425df588446565529b4e8b8e41945eeb
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-12-09 12:43:29 +0500

    section ui add

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,3 @@
-import { ProductCard } from '@/ui';
-
 export { CustomButton } from './button/button';
 export { CategoryCard } from './card/category-card';
 export { ProductCard } from './card/product-card';
@@ -8,4 +6,3 @@ export { Loading } from './loading';
 export { PageTitle } from './page-title/page-title';
 export { SectionTitle } from './section-title/section-title';
 export { CustomText } from './text/text';
-
