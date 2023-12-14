802cc28d65a3374608d3eeb17243d042a9a86946
husanovjamshid <husanovjamshid11@gmail.com>
2023-12-14 15:31:27 +0500

    change stat page

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -3,3 +3,7 @@ export { CustomButton } from './button/custom';
 export { ButtonWithArrow } from './button/with-arrow/index';
 export { ProductCard } from './card/product';
 export * from './icons';
+export { InputField } from './input/input';
+export { Loading } from './loading';
+export { PageTitle } from './page-title';
+export { CustomText } from './text/text';
