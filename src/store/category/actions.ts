0fa263849a0d811a0fff91589c0f3352f553679f
husanovjamshid <husanovjamshid11@gmail.com>
2023-12-18 22:22:05 +0500

    change category store

--- src/store/category/actions.ts
+++ src/store/category/actions.ts
@@ -1,17 +1,19 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { addNotification, errorCatch } from '../../utils';
-import { IAuthResponse } from './interface';
-import { CategoryService } from '@/services/category';
+import { CategoryService } from "@/services";
+import { addNotification, errorCatch } from "@/utils";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { IArgCategory } from "./interface";
 
-export const Category = createAsyncThunk<IAuthResponse>('category/all', async ({ callback }, thunkApi) => {
-	try {
-		const response = await CategoryService.getAll();
-		if (response.data) {
-			callback(response.data);
-		}
-		return response.data;
-	} catch (error) {
-		addNotification(error);
-		return thunkApi.rejectWithValue({ error: errorCatch(error) });
-	}
-});
+export const getAllCategory = createAsyncThunk<any, IArgCategory>(
+  "get/all-category",
+  async ({ callback }, thunkApi) => {
+    try {
+      const response = await CategoryService.getAll();
+      if (response.data) {
+        callback(response.data);
+      }
+    } catch (error) {
+      addNotification(error);
+      return thunkApi.rejectWithValue({ error: errorCatch(error) });
+    }
+  }
+);
